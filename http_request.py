8bcad5bfe31d040cec2295a998a6c01dff5c0861
Svetlana Solodovnikova <svslight@yandex.ru>
2019-02-13 13:13:04 +0500

    Добавлен менеджер контекста, параметр from_lang, функция для объединения пути os.path.join

--- http_request.py
+++ http_request.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
 API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(arg_text, to_lang):
+def translate_it(arg_text, to_lang, from_lang):
     """
     https://translate.yandex.net/api/v1.5/tr.json/translate ?
     key=<API-ключ>
@@ -18,25 +19,22 @@ def translate_it(arg_text, to_lang):
     params = {
         'key': API_KEY,
         'text': arg_text,
-        'lang': '{}-ru'.format(to_lang),
-        # 'lang': 'ru-{}'.format(to_lang),
+        'lang': '{}-{}'.format(to_lang, from_lang),
     }
     response = requests.get(URL, params=params)
     json_ = response.json()
     return ' '.join(json_['text'])
 
 
-def translate(path_in, lang='ru'):
-    open_file = open(path_in)
-    read_file = open_file.read()
-    name = f'{path_in}'
-    translate_file = translate_it(read_file, lang)
-
-    with open(f'translated/{name}', 'w', encoding='utf8') as f:
-        print('Запись перевода в файл:', name)
-        f.write(translate_file)
+def translate(path_in, to_lang, from_lang='ru'):
+    with open(path_in) as file_t:
+        name = f'{path_in}'
+        translate_file = translate_it(file_t, to_lang, from_lang)
+        with open(os.path.join(r'd:', '\Projects', 'Netology', 'work_http_requests', 'translated', 'name'), 'w', encoding='utf8') as f:
+            print('Запись перевода в файл:', name)
+            f.write(translate_file)
 
 
-translate('DE.txt', 'de')
+translate('DE.txt', 'de', 'en')
 translate('ES.txt', 'es')
-translate('FR.txt', 'fr')
+translate('FR.txt', 'fr', 'zh')
